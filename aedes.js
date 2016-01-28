645f479fab19a167d0b065b0eee440053f792876
ValessioBrito <contato@valessiobrito.com.br>
2016-01-28 04:25:58 -0200

    Animando sentido inverso

--- aedes.js
+++ aedes.js
@@ -1,9 +1,9 @@
 $(window).ready(function() {
 
-	$('head').append('<style type="text/css">div#zona-voo { position: absolute; top: 0px; left: 0px; height:300px; width:100%; } .aegypti { display: block; position:fixed; width: 50px; height: 60px; margin: 2% auto; background: url("http://valessiobrito.github.io/viral-aedes-aegypti/aedes.png") left center; animation: play .2s steps(6) infinite; z-index: 10; } @keyframes play { 100% { background-position: -300px; } }</style>');
+	$('head').append('<style type="text/css">.flipAE { transform: scaleX(-1); } div#zona-voo { position: absolute; z-index:9999; top: -300px; left: 0px; height:300px; width:70%; } .aegypti { display: block; position:fixed; width: 60px; height: 50px; margin: 2% auto; background: url("http://valessiobrito.github.io/viral-aedes-aegypti/aedes.png") left center; animation: play .2s steps(6) infinite; z-index: 10; } @keyframes play { 100% { background-position: -360px; } } .rotateAE { animation: playRotate .3s steps(4) infinite; } @keyframes playRotate { 100% { background-position: 240px; } }</style>');
 
 	$(document).ready(function() {
-		$(document.body).append('<div id="zona-voo"><a href="http://combateaedes.saude.gov.br/" target="_blank" class="aegypti mosq1"></a><a href="http://combateaedes.saude.gov.br/" target="_blank" class="aegypti mosq2"></a><a href="http://combateaedes.saude.gov.br/" target="_blank" class="aegypti mosq3"></a></div>');
+		$(document.body).append('<div id="zona-voo"><a href="http://combateaedes.saude.gov.br/" target="_blank" class="aegypti"></a><a href="http://combateaedes.saude.gov.br/" target="_blank" class="aegypti"></a><a href="http://combateaedes.saude.gov.br/" target="_blank" class="aegypti"></a></div>');
 });
 
 	$(document).ready(function() {
@@ -31,15 +31,18 @@ $(window).ready(function() {
 	    var oldq = $target.offset();
 	    var origAngle = $target[0].angle || 0;
 	    var angle = ((Math.atan2(newq.top-oldq.top, newq.left-oldq.left)/Math.PI) * 180 ) - 90;
-	    $target[0].angle = angle;
 
-      // jQuery::animate wont work for transform, so we do it by computing frames:
+      // .flipAE é a animação no sentido contrario, usar somente .aegypti é animação sentido normal
+      // .rotateAE são 4 frames para rotação, deve reproduzir somente no limite/retorno 
       for ( var step=0; step<=1; step+=.1 ) {
-          (function (stepAngle) {
+	setTimeout(function(){
+		$target.removeClass('aegypti flipAE').addClass('aegypti')
+	}, 50*step);
+        (function (stepAngle) {
               setTimeout(function(){
-                  $target.css({transform: 'rotate('+(origAngle+stepAngle)+'deg)'})
-              }, 600*step);
-          })((angle-origAngle)*step);
+		   $target.removeClass('aegypti rotateAE').addClass('aegypti flipAE')
+              }, 50*step);
+        })($target.removeClass('aegypti flipAE').addClass('aegypti rotateAE'), 300*step);
       }
 
 	    var speed = calcSpeed(oldq, newq);
@@ -59,7 +62,7 @@ $(window).ready(function() {
 	    //var speedModifier = 0.1;
 	    //var speed = Math.ceil(dist / speedModifier);
 	    //return speed;
-	    return Math.log(dist/10 + 1) * 1000;
+	    return Math.log(dist/10 + 1) * 900;
 	}
 
 });
